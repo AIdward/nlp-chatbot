e328ca372463f0f6f4278326319c220aa4f9875d
SunwoongShin <dev.ios.edward@gmail.com>
2021-05-06 11:32:07 +0900

    Initial commit

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,6 +1,7 @@
 import nltk
 nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 
 stemmer = PorterStemmer()
 
@@ -11,10 +12,16 @@ def stem(word):
     return stemmer.stem(word.lower())  
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
-
-words = ["organize", "organizes", "organizing", "organizer"]
-
-stemmed_words = [stem(w) for w in words]
-
-print(stemmed_words)
+    """
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag = [0, 1, 0, 1, 0, 0, 0]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+            
+    return bag
